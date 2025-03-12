e699365f9566d167344ff1a16ce6799212b24b0e
ablaye seck <seckablaye519@gmail.com>
2025-03-12 10:28:56 +0000

    remerciement

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Définir EJS comme moteur de template
 app.set('view engine', 'ejs');
 
-// Middleware pour les fichiers statiques
 app.use(express.static('public'));
 
-// Route principale
 app.get('/', (req, res) => {
     res.render('index', { title: 'Bienvenue sur Notre Application' });
 });
 
-// Démarrer le serveur
 app.listen(PORT, () => {
-    console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+    console.log(` Serveur démarré sur http://localhost:${PORT}`);
 });
